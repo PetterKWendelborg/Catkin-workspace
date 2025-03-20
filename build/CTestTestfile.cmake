c70dbe3a8c809a0d3e1ea42f6e6c5192300134bb
PetterKWendelborg <Petter.k.Wendelborg@gmail.com>
2025-03-20 10:59:26 +0100

    Initial commit of deep_ocean package

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/petter/UUV_ws/src/deep_ocean
+# Build directory: /home/petter/UUV_ws/src/deep_ocean/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
