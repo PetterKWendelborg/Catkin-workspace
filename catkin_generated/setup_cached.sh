86603fd80b1acbadfbf99d2c6e683e7befba54f3
GitZondre <sondre.haland@outlook.com>
2025-04-02 13:42:36 +0200

    Add files via upload

---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/petter/UUV_ws/src/deep_ocean/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/petter/UUV_ws/src/deep_ocean/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/petter/UUV_ws/src/deep_ocean/build/devel/lib:/opt/ros/noetic/lib:/usr/lib/x86_64-linux-gnu'
+export PATH='/opt/ros/noetic/bin:/home/petter/.local/bin:/home/petter/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/petter/UUV_ws/src/deep_ocean/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/petter/UUV_ws/src/deep_ocean/build'
+export ROS_PACKAGE_PATH="/home/petter/UUV_ws/src/deep_ocean:$ROS_PACKAGE_PATH"
