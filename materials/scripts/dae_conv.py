64fd61a4b6ae2a5e5e2aef716060ea4cd07e6b80
GitZondre <sondre.haland@outlook.com>
2025-04-17 20:27:46 +0200

    Add files via upload

---
+++ materials/scripts/dae_conv.py
@@ -0,0 +1,46 @@
+import bpy
+import os
+from math import sqrt
+
+# Clear existing objects
+bpy.ops.wm.read_factory_settings(use_empty=True)
+
+# Settings
+marker_path = "marker_0.png"  # Change this
+output_dae = "marker_0.dae"  # Change this
+marker_size = 1.0  # Base size in meters (will be applied to the longer dimension)
+
+# Load image to get dimensions
+image = bpy.data.images.load(marker_path)
+width, height = image.size
+aspect_ratio = width / height
+
+# Create plane with correct proportions
+if width > height:
+    plane_width = marker_size
+    plane_height = marker_size / aspect_ratio
+else:
+    plane_height = marker_size
+    plane_width = marker_size * aspect_ratio
+
+bpy.ops.mesh.primitive_plane_add(size=1.0)
+plane = bpy.context.object
+plane.scale.x = plane_width
+plane.scale.y = plane_height
+
+# Add material with texture
+mat = bpy.data.materials.new(name="MarkerMaterial")
+mat.use_nodes = True
+bsdf = mat.node_tree.nodes["Principled BSDF"]
+tex_image = mat.node_tree.nodes.new('ShaderNodeTexImage')
+tex_image.image = image
+mat.node_tree.links.new(bsdf.inputs['Base Color'], tex_image.outputs['Color'])
+
+# Assign material to plane
+if plane.data.materials:
+    plane.data.materials[0] = mat
+else:
+    plane.data.materials.append(mat)
+
+# Export as DAE
+bpy.ops.wm.collada_export(filepath=output_dae)
