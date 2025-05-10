f22cf1574ad96c104f5bc5ee3cb3aa5ee6e29c3b
Dragonboom50 <31338481+Dragonboom50@users.noreply.github.com>
2025-05-10 20:29:34 +0200

    Add files via upload

---
+++ rov_control/rov_control_scripts/plots/plot_heading_angle_yaw_rov.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3 
+import rospy
+import math
+from sensor_msgs.msg import Imu
+from matplotlib import pyplot as plt
+from std_msgs.msg import Bool
+from geometry_msgs.msg import PointStamped
+
+alignment_heading_time_list_angular_velocity = []
+correction_heading_time_list_angular_velocity = []
+alignment_heading_angular_velocity_yaw_list = []
+correction_heading_angular_velocity_yaw_list = []
+
+alignment_heading_time_list_heading_angle = []
+correction_heading_time_list_heading_angle = []
+alignment_heading_angle_yaw_list= []
+correction_heading_angle_yaw_list = []
+
+start_plotting_outer_rov_heading = False
+stop_plotting_outer_rov_heading = False
+start_plotting_inner_rov_heading = False
+stop_plotting_inner_rov_heading = False
+display_plot = False
+
+def heading_angular_velocity_callback(msg):
+    global alignment_heading_time_list_angular_velocity
+    global correction_heading_time_list_angular_velocity
+    global alignment_heading_angular_velocity_yaw_list
+    global correction_heading_angular_velocity_yaw_list
+
+    imu_time = msg.header.stamp.to_sec()
+    angular_velocity_z = msg.angular_velocity.z
+    angular_velocity_z_deg = math.degrees(angular_velocity_z)
+
+    if start_plotting_outer_rov_heading and not stop_plotting_outer_rov_heading:
+        alignment_heading_time_list_angular_velocity.append(imu_time)
+        alignment_heading_angular_velocity_yaw_list.append(angular_velocity_z_deg)
+
+    if start_plotting_inner_rov_heading and not stop_plotting_inner_rov_heading:
+        correction_heading_time_list_angular_velocity.append(imu_time)
+        correction_heading_angular_velocity_yaw_list.append(angular_velocity_z_deg)
+
+
+def heading_angle_callback(msg):
+    global alignment_heading_time_list_heading_angle
+    global correction_heading_time_list_heading_angle
+    global alignment_heading_angle_yaw_list
+    global correction_heading_angle_yaw_list
+    
+    heading_angle_time = msg.header.stamp.to_sec()
+    heading_angle_value = msg.point.z
+    
+    if start_plotting_outer_rov_heading and not stop_plotting_outer_rov_heading:
+        alignment_heading_time_list_heading_angle.append(heading_angle_time)
+        alignment_heading_angle_yaw_list.append(heading_angle_value)
+
+    if start_plotting_inner_rov_heading and not stop_plotting_inner_rov_heading:
+        correction_heading_time_list_heading_angle.append(heading_angle_time)
+        correction_heading_angle_yaw_list.append(heading_angle_value)
+
+def plot_data():
+
+    fig, ax = plt.subplots(2,2, figsize= (14,8), sharey = False)
+    fig.canvas.set_window_title("plot_heading_angle_yaw_rov.py")
+    fig.suptitle("")
+
+    ax[0,0].set_title("ROV alignment heading - angular velocity in yaw")
+    ax[0,0].set_ylabel("angular velocity (deg/sec)")
+    ax[0,0].set_xlabel("simulation time (sec)")
+    ax[0,0].grid()
+    ax[0,0].legend(loc = "upper left")
+
+    ax[1,0].set_title("ROV correction heading - angular velocity in yaw")
+    ax[1,0].set_ylabel("angular velocity (deg/sec)")
+    ax[1,0].set_xlabel("simulation time (sec)")
+    ax[1,0].grid()
+    ax[1,0].legend(loc = "upper left")
+
+
+    ax[0,1].set_title("ROV alignment heading yaw - heading angle")
+    ax[0,1].set_ylabel("angle (deg)")
+    ax[0,1].set_xlabel("simulation time (sec)")
+    ax[0,1].grid()
+    ax[0,1].legend(loc = "upper left")
+
+    ax[1,1].set_title("ROV correction heading - heading angle")
+    ax[1,1].set_ylabel("angle (deg)")
+    ax[1,1].set_xlabel("simulation time (sec)")
+    ax[1,1].grid()
+    ax[1,1].legend(loc = "upper left")
+
+    ax[0,0].plot(alignment_heading_time_list_angular_velocity, alignment_heading_angular_velocity_yaw_list, color = "black", marker = "." , label = "")
+    ax[1,0].plot(correction_heading_time_list_angular_velocity, correction_heading_angular_velocity_yaw_list, color = "green", marker = "." , label = "")
+
+    ax[0,1].plot(alignment_heading_time_list_heading_angle, alignment_heading_angle_yaw_list, color = "black", marker = "." , label = "")
+    ax[1,1].plot(correction_heading_time_list_heading_angle, correction_heading_angle_yaw_list, color = "green", marker = "." , label = "")
+    fig.tight_layout()
+
+    plt.show()
+
+def start_outer_heading_call(outer_rov_heading_start):
+    global start_plotting_outer_rov_heading
+    start_plotting_outer_rov_heading = outer_rov_heading_start.data
+
+def stop_outer_heading_call(outer_rov_heading_stop):
+    global stop_plotting_outer_rov_heading
+    stop_plotting_outer_rov_heading = outer_rov_heading_stop.data
+        
+def start_inner_heading_call(inner_rov_heading_start):
+    global start_plotting_inner_rov_heading
+    start_plotting_inner_rov_heading = inner_rov_heading_start.data
+
+def stop_inner_heading_call(inner_rov_heading_stop):
+    global stop_plotting_inner_rov_heading
+    stop_plotting_inner_rov_heading = inner_rov_heading_stop.data
+
+def plot_call(docking_done):
+    global display_plot
+    display_plot = docking_done.data
+    if display_plot:
+        plot_data()
+        rospy.signal_shutdown("Condition met")
+
+if __name__ == "__main__":
+    rospy.init_node("rov_heading_angle_and_angular_velocity")
+
+    rospy.Subscriber("/rov/imu", Imu, heading_angular_velocity_callback)
+    rospy.Subscriber("/rov/tms_center_angle", PointStamped, heading_angle_callback)
+
+    rospy.Subscriber("/tms/heading_done", Bool, start_outer_heading_call)
+    rospy.Subscriber("/rov/heading_done", Bool, stop_outer_heading_call)
+    rospy.Subscriber("/tms/inner_heading_done", Bool, start_inner_heading_call)
+    rospy.Subscriber("/rov/inner_heading_done", Bool, stop_inner_heading_call)
+
+    rospy.Subscriber("/rov/docking_done", Bool, plot_call)
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        plot_data()
---
+++ tms_control/tms_control_scripts/3d_sonar_scripts/inner_heading_3d_advanced_tms.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3 
+import rospy
+import math
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Point, Wrench, PointStamped
+
+# This code subscribes to the /tms/rov_center topic which gets its information from ponitcloud_to_xyz_3d_tms.py
+# here the sonar data is finaly used to adjust the TMS's heading towards the ROV
+
+tms_inner_heading_ready = False
+last_time_in_window = None
+
+def center_call(center_msg, pub):
+    center_x = center_msg.point.x
+    center_z = center_msg.point.z
+    inner_hyst_window = 0.5
+    outer_hyst_window = 10.0
+    hysteresis_duration = 4
+    now = rospy.get_time()
+
+    inner_torque = 0.1
+    outer_torque = 1.0
+
+    global last_time_in_window
+    global tms_inner_heading_ready
+    if tms_inner_heading_ready:
+        
+        if center_z != 0:
+            angle_to_center = math.asin(center_x/center_z)
+        else:
+            angle_to_center = 0
+
+        angle_to_center_gaz_deg = math.degrees(angle_to_center)
+        # rospy.loginfo(f"angle to center = {angle_to_center_gaz} rad")  
+        # rospy.loginfo(f"angle to center = {math.degrees(angle_to_center_gaz)} deg") 
+
+        tms_wrench_msg = Wrench()
+        boolean = Bool()
+
+        if angle_to_center_gaz_deg >= -inner_hyst_window and angle_to_center_gaz_deg <= inner_hyst_window:
+            tms_wrench_msg.torque.z = 0
+
+            if last_time_in_window is None:
+                last_time_in_window = now
+
+            elif now - last_time_in_window >= hysteresis_duration:
+                # Stayed in window for enough time
+                rospy.loginfo("plz_move_forward_rov")
+                boolean.data = True
+                condition_pub.publish(boolean)
+                rospy.signal_shutdown("Condition met")
+
+        # If the angle is less than -outer_hyst_window
+        elif angle_to_center_gaz_deg <= -outer_hyst_window:
+            tms_wrench_msg.torque.z = outer_torque
+            last_time_in_window = None
+
+        # If the angle is betweem -outer_hyst_window and -inner_hyst_window 
+        elif angle_to_center_gaz_deg > -outer_hyst_window and angle_to_center_gaz_deg < -inner_hyst_window :
+            tms_wrench_msg.torque.z = inner_torque
+            last_time_in_window = None
+
+        # If the angle is higher than outer_hyst_window
+        elif angle_to_center_gaz_deg >= outer_hyst_window:
+            tms_wrench_msg.torque.z = -outer_torque
+            last_time_in_window = None
+
+        # If the angle is betweem outer_hyst_window and inner_hyst_window 
+        elif angle_to_center_gaz_deg < outer_hyst_window and angle_to_center_gaz_deg > inner_hyst_window :
+            tms_wrench_msg.torque.z = -inner_torque
+            last_time_in_window = None
+
+        else:
+            rospy.loginfo("else statement")
+            tms_wrench_msg.torque.z = 0
+
+        pub.publish(tms_wrench_msg)
+    
+def condition_call(approach_done):
+    global tms_inner_heading_ready
+    if approach_done.data:
+        tms_inner_heading_ready = approach_done.data
+
+if __name__ == "__main__":
+    rospy.init_node("tms_inner_heading")
+    
+    cmd_vel_pub = rospy.Publisher("/tms/thruster_manager/input", Wrench, queue_size = 10)
+
+    rospy.Subscriber("/tms/rov_center", PointStamped, center_call, cmd_vel_pub)
+    rospy.Subscriber("/rov/approach_done", Bool, condition_call)
+
+    condition_pub = rospy.Publisher("/tms/inner_heading_done", Bool, queue_size = 10)
+
+    rospy.spin()
